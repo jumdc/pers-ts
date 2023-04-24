78ef984796c23ffdd4aeb5fa0f6a7d3500edd9db
jumdc <julie.mordacq@live.fr>
2023-04-24 15:02:40 +0200

    add exploration of filtrations and PH

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     filtration = Filtration([-10, 0, 5, 0, 0, 19, 0, 0])
+    plt.plot(filtration.data)
     filtration.compute_persistence()
-    ax = filtration._plot_persistence_diagram()
-    plt.savefig("test.png")
+
+    print(filtration.persistence)
+
+    # ax = filtration._plot_persistence_diagram()
+    # plt.savefig("test.png")
