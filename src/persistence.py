c39b0fcb4e3230c28b128dd60f540af5fd5aa5c9
jumdc <julie.mordacq@live.fr>
2023-04-24 17:31:51 +0200

    add persistence transform

---
+++ src/persistence.py
@@ -0,0 +1,107 @@
+"""Sub Level set filtration for 1-d Time Series"""
+
+
+import gudhi
+import numpy as np
+import gudhi.representations
+import matplotlib.pyplot as plt
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class Persistence(BaseEstimator, TransformerMixin):
+    """Class for Sub Level set filtration for 1-d Time Series"""
+    def __init__(self):
+        """Initialization
+        """
+        self.simplices = None
+        self.is_fitted = False
+
+    def fit(self, X, y=None):
+        """
+        Simplex tree fit 
+        
+        Returns
+        -------
+        simplices : list
+            List of simplices (n)
+        """
+        
+        self.simplices = [
+            self._create_simplex(to_filter)
+            for to_filter in X
+        ]
+        self.is_fitted = True
+        return self
+
+    @staticmethod
+    def _create_simplex(data):
+        """
+        Creates a simplex tree from the 1-d data. 
+
+        Parameters
+        ----------
+        data : list
+            1-d data
+        
+        Returns
+        -------
+        simplex : gudhi.SimplexTree
+            Simplex tree
+        """
+        simplex = gudhi.SimplexTree()
+        # First we insert the vertices of the time series
+        for idx, value in enumerate(data):
+            simplex.insert(
+                [idx], # 0-simplex
+                filtration=value # the ordering on our vertices
+                # is the values of the time series
+            )
+        # Second, we insert the edges of the time series
+        for idx, value in enumerate(data):
+            simplex.insert(
+                [idx, idx+1], # 1-simplex
+                filtration=value
+            )
+        return simplex
+
+
+    def transform(self, X, y=None):
+        """
+        Computes the persistence intervals for each filtration
+        
+        Returns
+        -------
+        res : list
+            List of persistence intervals for each filtration
+        """
+        res = [
+            self.compute_persistence(simplex)
+            for simplex in self.simplices
+        ]
+        return res
+
+
+    @staticmethod
+    def compute_persistence(simplex):
+        """
+        Computes persistence
+        
+        Parameters
+        ----------
+        graph : bool, optional
+            If True, plots persistence diagram and persistence barcode, by default False
+
+        Returns
+        -------
+        persistence : list
+            List of persistence pairs
+        """
+        persistence = simplex.persistence()
+        maxi = max([filtration_val[1] for filtration_val in list(simplex.get_filtration())])
+        persistence = np.asarray(
+            [
+                [ele[1][0], ele[1][1]] 
+                if ele[1][1] < np.inf else [ele[1][0], maxi]
+                for ele in persistence 
+                ]
+        )
+        return persistence
---
+++ src/test/test_persistence.py
@@ -0,0 +1,48 @@
+import unittest
+import numpy as np
+import pyrootutils
+
+
+root = pyrootutils.setup_root(
+    search_from=__file__,
+    indicator=[".git", "pyproject.toml"],
+    pythonpath=True,
+    dotenv=True,
+)
+
+from src.persistence import Persistence
+
+class TestPersistence(unittest.TestCase):
+    def test_persistence_one_data(self):
+        data = [
+            [6, 9, 7, 4, 5, 7, 8, 2, 1, 2]
+        ]
+        persistence = Persistence()
+        res = persistence.fit_transform(data)
+
+
+        gt = np.asarray([[1.0, 9], [4.0, 8.0], [6.0, 9.0]])
+        self.assertTrue(
+            np.array_equal(res[0], gt)
+        )
+
+    
+    def test_persistence_several(self):
+        data = [
+            [6, 9, 7, 4, 5, 7, 8, 2, 1, 2],
+            [0, 0, 3, 8, 7, 5, 5, 0, 4, 3]
+        ]
+        persistence = Persistence()
+        res = persistence.fit_transform(data)
+
+        gt = np.asarray([
+            [[1.0, 9], [4.0, 8.0], [6.0, 9.0]],
+            [[0.0,8.0], [0.0,8.0], [3.0,4.0]]
+            ])
+        self.assertTrue(
+            np.array_equal(res, gt)
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
